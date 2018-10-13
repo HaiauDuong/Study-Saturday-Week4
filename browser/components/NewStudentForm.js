f4a78b3dacdfc576d20ac06568a3a18164ebfae7
Haiau Duong <duong.h.pgd@gmail.com>
2018-10-13 12:01:56 -0400

    Cycle 3 Complete

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class NewStudentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      firstName: "",
+      lastName: "",
+      email: ""
+    });
+  }
+
   render() {
+    console.log(this.state)
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input onChange={this.handleChange} value={this.state.firstName}type="text" name="firstName" />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input onChange={this.handleChange} value={this.state.lastName}type="text" name="lastName" />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input onChange={this.handleChange} value={this.state.email}type="email" name="email" />
         </label>
 
         <button type="submit">Submit New Student</button>
